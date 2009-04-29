8e0c3428fe77dfc9d70d03ac38e69525a3703f0b
ottokang <ottokang@3349bf3a-1dcd-11de-bcda-2bb9902f8029>
2009-04-29 14:54:15 +0000

    - 加入Google Analytics的追蹤設定

--- current/template/layouts/default.php
+++ current/template/layouts/default.php
@@ -52,5 +52,15 @@ include ROOT_DIR . 'template/layouts/components/headTitle.php';
             </div>
         </div>
         <?php echo $this->headScript() ?>
+        <script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8577883-1");
+pageTracker._trackPageview();
+} catch(err) {}
+</script>
     </body>
 </html>
