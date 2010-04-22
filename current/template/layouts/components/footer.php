4615be137a37d8cde7f2af3c2dd1b41321e40692
ottokang <ottokang@3349bf3a-1dcd-11de-bcda-2bb9902f8029>
2010-04-22 09:30:53 +0000

    - 調整瀏覽器的順序，提昇Google Chrome的位置

--- current/template/layouts/components/footer.php
+++ current/template/layouts/components/footer.php
@@ -18,9 +18,9 @@
         建議使用瀏覽器：
         <?php echo $this->img('browserLogo/firefox.png', 'Firefox') ?><a href="http://moztw.org/">Firefox</a>、
         <?php echo $this->img('browserLogo/ie7.png', 'ie7') ?><a href="http://www.microsoft.com/taiwan/products/ie/">IE 8</a>、
+        <?php echo $this->img('browserLogo/googleChrome.png', 'GoogleChrome') ?><a href="http://www.google.com/chrome">Google Chrome</a>、
         <?php echo $this->img('browserLogo/safari.png', 'Safari') ?><a href="http://www.apple.com/tw/safari/">Safari</a>、
-        <?php echo $this->img('browserLogo/opera.png', 'Opera') ?><a href="http://www.opera.com/">Opera</a>、
-        <?php echo $this->img('browserLogo/googleChrome.png', 'GoogleChrome') ?><a href="http://www.google.com/chrome">Google Chrome</a>
+        <?php echo $this->img('browserLogo/opera.png', 'Opera') ?><a href="http://www.opera.com/">Opera</a>
         |
         <a target="_blank" class="external" href="http://code.google.com/p/auti-elementary-school-website/">程式專案網站</a>&nbsp;
         <a target="_blank" class="external" href="http://validator.w3.org/check?uri=referer">XHTML 驗證</a>
