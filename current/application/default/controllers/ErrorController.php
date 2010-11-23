899158b5fa7e6e670027dbae8fd28ef60e9a5e70
ottokang@gmail.com <ottokang@gmail.com@3349bf3a-1dcd-11de-bcda-2bb9902f8029>
2010-11-23 11:15:38 +0000

    - 修正錯誤訊息

--- current/application/default/controllers/ErrorController.php
+++ current/application/default/controllers/ErrorController.php
@@ -25,16 +25,16 @@ class ErrorController extends Controller
         // 設定Layout
         $this->_helper->layout->setLayout('error');
         $errors = $this->getParam('error_handler');
-
+        //print_r($errors);exit;
         switch ($errors->type) {
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
-                //網址錯誤
+                // 網址錯誤
                 $this->view->message = '抱歉！您輸入的網址不存在';
                 break;
             default:
-                //無權限存取這個網址
-                $this->view->message = 'Sorry！無權限存取這個網址';
+                // 無法存取網址
+                $this->view->message = '無法存取這個網址，請洽系統管理員';
                 break;
         }
 
